0bf6ad6014a4291055fd97292eeb8e383bd45073
Vitalii Sharapov <Vitalii_Sharapov@epam.com>
2017-07-05 17:02:16 +0300

    Refactor. Update homework

--- src/main/java/ua/rd/ioc/ApplicationContext.java
+++ src/main/java/ua/rd/ioc/ApplicationContext.java
@@ -1,12 +1,17 @@
 package ua.rd.ioc;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 /**
+ * Considered that bean has only one construct - non arg or with arg but not
+ * both of them
+ * <p>
  * Created by Vitalii_Sharapov on 7/3/2017.
  */
 public class ApplicationContext implements Context {
@@ -20,6 +25,21 @@ public class ApplicationContext implements Context {
 
     public ApplicationContext(Config config) {
         this.config = config;
+
+        if (config != null) {
+            initApplicationContext();
+        }
+    }
+
+    private void initApplicationContext() {
+        BeanDefinition[] beanDefinitions = config.getBeanDefinitions();
+
+        for (BeanDefinition beanDefinition : beanDefinitions) {
+            String beanName = beanDefinition.getBeanName();
+
+            // TODO: 7/5/2017 ignore prototypes
+            createBean(beanName);
+        }
     }
 
     @Override
@@ -32,20 +52,10 @@ public class ApplicationContext implements Context {
 
     @Override
     public <T> T getBean(String beanName) {
-        T bean = null;
-
-        if (context.containsKey(beanName)) {
-            bean = (T) context.get(beanName);
-            return bean;
-        }
-        bean = createBeanWithNoArgConstructor(beanName);
-        callInitMethod(bean);
-
-        // TODO: 7/5/2017 if prototype then do not put to context. also do not
+        // TODO: 7/5/2017 if prototype create bean
         // call destroy on prototype because it is not in context
-        context.put(beanName, bean);
 
-        return bean;
+        return (T) context.get(beanName);
     }
 
     private <T> void callInitMethod(T bean) {
@@ -61,13 +71,37 @@ public class ApplicationContext implements Context {
         }
     }
 
-    private <T> T createBeanWithNoArgConstructor(String beanName) {
-        return (T) Arrays.stream(config.getBeanDefinitions())
+    private void createBean(String beanName) {
+        if (context.containsKey(beanName)) {
+            return;
+        }
+
+        // TODO: 7/5/2017 consider prototype
+        BeanDefinition beanDefinition = getBeanDefinition(beanName);
+        Class<?> beanType = beanDefinition.getType();
+        Constructor<?> beanConstructor = beanType.getConstructors()[0];
+        Object bean = null;
+        if (beanConstructor.getParameterCount() == 0) {
+            bean = createBeanWithNoArgConstructor(beanType);
+        } else {
+            // TODO: 7/5/2017 implement
+            // bean = createBeanWithArgConstruct();
+        }
+
+        callInitMethod(bean);
+
+        context.put(beanName, bean);
+    }
+
+    private BeanDefinition getBeanDefinition(String beanName) {
+        return Arrays.stream(config.getBeanDefinitions())
                 .filter(e -> e.getBeanName().equals(beanName))
                 .findAny()
-                .map(b -> b.getType())
-                .map(c -> newInstance(c))
-                .orElse(null);
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    private <T> T createBeanWithNoArgConstructor(Class<T> beanType) {
+        return newInstance(beanType);
     }
 
     private <T> T newInstance(Class<?> clazz) {
